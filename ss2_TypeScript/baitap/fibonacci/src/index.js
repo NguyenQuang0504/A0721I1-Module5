b83f9271aacb056787aa60b360833bac0c6c3cc9
NguyenQuang0504 <quang10a30504@gmail.com>
2022-04-24 15:07:56 +0700

    update bai tap

--- ss2_TypeScript/baitap/fibonacci/src/index.js
+++ ss2_TypeScript/baitap/fibonacci/src/index.js
@@ -1,13 +1,14 @@
 "use strict";
 exports.__esModule = true;
+// function tra ve cac so fibonacci dau tien
 function fibonacci(count) {
     var a = 1;
-    var b = 2;
+    var b = 1;
     var c;
     var array = [];
     array.push(a);
     array.push(b);
-    for (var i = 3; i <= count; i++) {
+    for (var i = 2; i < count; i++) {
         c = a + b;
         array.push(c);
         a = b;
@@ -21,4 +22,16 @@ for (var i = 0; i < array.length; i++) {
     sum += array[i];
 }
 console.log("Cac so fibonacci la " + array);
-console.log("tong cua 10 so fibonacci dau tien la" + sum);
+console.log("tong cua 10 so fibonacci dau tien la " + sum);
+// Su dung de quy
+function fibonacci1(n) {
+    if (n == 1 || n == 2)
+        return 1;
+    return fibonacci1(n - 1) + fibonacci1(n - 2);
+}
+var sum2 = 0;
+for (var i = 1; i <= 10; i++) {
+    sum2 += fibonacci1(i);
+    console.log(fibonacci1(i));
+}
+console.log("Tong cac so fibonacci la" + sum2);
--- ss2_TypeScript/baitap/fibonacci/src/index.ts
+++ ss2_TypeScript/baitap/fibonacci/src/index.ts
@@ -2,12 +2,12 @@ import {finished} from "stream";
 // function tra ve cac so fibonacci dau tien
 function  fibonacci(count:number) {
     let a:number =1;
-    let b:number =2;
+    let b:number =1;
     let c:number;
     let array:Array<number>=[];
     array.push(a);
     array.push(b);
-    for (let i:number=3;i<=count;i++){
+    for (let i:number=2;i<count;i++){
         c=a+b;
         array.push(c);
         a=b;
@@ -22,3 +22,15 @@ for (let i:number=0;i<array.length;i++){
 }
 console.log("Cac so fibonacci la " +array);
 console.log("tong cua 10 so fibonacci dau tien la " +sum);
+
+// Su dung de quy
+function fibonacci1(n:number):number{
+    if (n == 1 || n == 2) return 1;
+    return fibonacci1(n-1) + fibonacci1(n-2);
+}
+let sum2:number=0;
+for (let i:number=1;i<=10;i++){
+    sum2+=fibonacci1(i);
+    console.log(fibonacci1(i));
+}
+console.log("Tong cac so fibonacci la" +sum2)
