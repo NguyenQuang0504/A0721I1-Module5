ea4a68cd6db33cb8c6bbd7b9dccf626a34e8ed61
NguyenQuang0504 <quang10a30504@gmail.com>
2022-04-24 09:31:41 +0700

    update bai tap

---
+++ ss3_angular/thuchanh/thong_tin_thu_nuoi/AngularOverview/src/app/profile/profile.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {IPet} from "./IPet";
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  pet: IPet = {
+    name: 'puppie',
+    imgLink: 'http://yourdost-blog-images.s3-ap-southeast-1.amazonaws.com/wp-content/uploads/2016/01/03165939/Dogs.jpg'
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
