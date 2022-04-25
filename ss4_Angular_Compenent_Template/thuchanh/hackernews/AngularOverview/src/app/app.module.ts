0be2a720311c982a0ecb72a20671a95cc8257358
NguyenQuang0504 <quang10a30504@gmail.com>
2022-04-25 11:52:23 +0700

    update

---
+++ ss4_Angular_Compenent_Template/thuchanh/hackernews/AngularOverview/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HackernewsComponent } from './hackernews/hackernews.component';
+import {FormsModule} from "@angular/forms";
+import { LikeComponent } from './like/like.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HackernewsComponent,
+    LikeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
